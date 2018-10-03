e6efc9021b4515d3d068252c7398f84826c48d14
ca33dang <38273466+ca33dang@users.noreply.github.com>
2018-10-03 09:39:30 -0400

    random price

--- randompizz.rb
+++ randompizz.rb
@@ -1,22 +1,11 @@
-def extra()
-   if extra == ["y"]
-   extra = ["five meats", "Hawain pizza", "chicken bacon ranch"].sample
-   else
-   puts ["n"]
-   end
-end
-def time()
-	time = 5
-end
-
-def price()
-	price = 10
+def pizza()
+	pizza = ["five cheese", "extra peperoni and cheese", "stuff crust with mozzerella"].sample
 end
 def size()
-	size = ["personal=1", "small=2", "medium=3", "large=4"].sample
+	size = ["personal pan", "small", "medium", "large"].sample
 end
 def crust()
-	crust = ["pan=1", "thin=2", "medium=3", "thick=4", "stuff=5"].sample
+	crust = ["pan", "thin", "medium", "thick", "stuff"].sample
 end
 def meat()
 	meat = ["peperoni", "sausage", "ham", "bacon", "meat-ball"].sample
@@ -24,13 +13,44 @@ end
 def veggies()
 	veggies = ["bell pepper", "pineapple", "tommatos", "banna pepper"].sample
 end
+def special()
+   if special == ["y"]
+   special = ["five meats", "Hawain pizza", "chicken bacon ranch"].sample
+   else
+   puts ["n"]
+   end
+end
+def price(size, special)
+	price = 0
+	if size == "personal pan"
+		price = 5
+	elsif size == "small"
+		price = 6
+	elsif size == "medium"
+		price = 7
+	elsif size == "large"
+		price = 9
+	end
+	if special == "five meats"
+		price += 5
+	elsif special == "Hawain pizza"
+		price += 7
+	elsif special == "chicken bacon ranch"
+		price += 4
+	end
+	return price
+end
+
+def time()
+	time = 15
+end
 puts "how many pizza do you want?"
 pizza = gets.chomp.to_i
 puts "do you want our specialty pizza? y or n"
-extra = gets.chomp.to_i
+special = gets.chomp.to_i
 pizza.times do
 
 
-puts "Your pizza size is #{size}, your crust is #{crust}, #{meat}, veggies #{veggies}, and #{extra}"
-puts "Your total is #{price} dollars, and your wait time is #{time} mins. "
+puts "Your pizza size is #{size}, your crust is #{crust}, #{meat}, veggies #{veggies}, and #{special}"
+puts "Your total is #{price(size, special)} dollars, and your wait time is #{time} mins. "
 end
